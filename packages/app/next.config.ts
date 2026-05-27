ac47940b5cb34b455c47737a701ec7b61e0dcbf9
Ennio <enniolopes@gmail.com>
2026-05-27 16:42:43 -0300

    fix: i18n

--- packages/app/next.config.ts
+++ packages/app/next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   transpilePackages: [
     '@cozsolidarias/data-gateway',
     '@cozsolidarias/data-source-static',
+    '@ttoss/react-i18n',
+    'react-intl',
+    '@formatjs/intl',
+    '@formatjs/icu-messageformat-parser',
+    'intl-messageformat',
   ],
 };
 
